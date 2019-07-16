810b202dde60e63365110d548ef4f53f34edceba
Yujohn Nattrass <Yujohn.natt@gmail.com>
2019-07-16 06:07:01 -0700

    pseudo code question 1

---
+++ rb101/lesson_2/pseudo_code_practice.rb
@@ -0,0 +1,19 @@
+# Question: a method that returns the sum of two integers
+
+# Casual pseudo
+# Given two numbers
+# save each number to a variable
+# add the two variables 
+#   - return the sum
+
+# Formal pseudo
+# START
+
+# Given two integers as arguments x and y
+
+# SET num1 = x
+# SET num2 = y
+
+# SET result = x + y
+# PRINT result
+# END
