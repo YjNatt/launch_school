79085d9ec140ae7dba3f483c5a57b5e2c94f8ad6
Yujohn Nattrass <Yujohn.natt@gmail.com>
2019-07-21 22:18:39 -0700

    RPS Bonus

--- rb101/lesson_2/rock_paper_scissors/rock_paper_scissors.rb
+++ rb101/lesson_2/rock_paper_scissors/rock_paper_scissors.rb
@@ -1,13 +1,33 @@
-VALID_CHOICES = %w(rock paper scissors)
+VALID_CHOICES = {
+  "r" => "rock",
+  "p" => "paper",
+  "s" => "scissors",
+  "l" => "lizard",
+  "S" => "Spock"
+}
+
+WHO_BEATS_WHO = {
+  "r" => ["s", "l"],
+  "p" => ["r", "S"],
+  "s" => ["p", "l"],
+  "S" => ["r", "s"],
+  "l" => ["S", "p"]
+}
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+def choices_prompt
+  choice_array = []
+  VALID_CHOICES.each do |key, value|
+    choice_array << "#{value}(#{key})"
+  end
+  choice_array
+end
+
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+  WHO_BEATS_WHO[first].include?(second)
 end
 
 def display_results(player, computer)
@@ -21,23 +41,43 @@ def display_results(player, computer)
 end
 
 loop do
-  choice = ''
+  player = 0
+  computer = 0
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = Kernel.gets().chomp()
+    choice = ''
+    loop do
+      prompt("Choose one: #{choices_prompt.join(', ')}")
+      choice = Kernel.gets().chomp()
 
-    if VALID_CHOICES.include?(choice)
+      if VALID_CHOICES.key?(choice)
+        break
+      else
+        prompt("That's not a valid choice")
+      end
+    end
+
+    computer_choice = VALID_CHOICES.values.sample[0]
+
+    prompt("You chose: #{VALID_CHOICES[choice]} " \
+          "Computer chose: #{VALID_CHOICES[computer_choice]}")
+
+    if win?(choice, computer_choice)
+      player += 1
+    elsif win?(computer_choice, choice)
+      computer += 1
+    end
+
+    display_results(choice, computer_choice)
+
+    prompt("Player score #{player} Computer score #{computer}")
+    if player == 5
+      prompt("You are the grand winner")
       break
-    else
-      prompt("That's not a valid choice")
+    elsif computer == 5
+      prompt("Computer is the grand winner")
+      break
     end
   end
-
-  computer_choice = VALID_CHOICES.sample
-
-  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
-
-  display_results(choice, computer_choice)
 
   prompt("Do you want to play again?")
   answer = Kernel.gets().chomp()
