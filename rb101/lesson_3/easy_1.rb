549e02c6f80a5524bc47dcd75ef3f9a2c62743ab
Yujohn Nattrass <Yujohn.natt@gmail.com>
2019-07-24 07:52:13 -0700

    easy 1 answers

---
+++ rb101/lesson_3/easy_1.rb
@@ -0,0 +1,48 @@
+=begin
+Question 1
+1
+2
+2
+3
+
+Question 2
+2.1 compares 2 values and returns true if they are not equal
+2.2 returns true if the value is 'nil' otherwise false
+2.3 is the name of the method
+2.4
+2.5 is the name of the method
+2.6 !! before something evaluates the expression to be true
+
+Question 3
+advice.gsub!("important", "urgent")
+
+Question 4
+numbers = [1, 2, 3, 4, 5]
+
+numbers.delete_at(1) will delete element at index 1 in the array
+  [1, 3, 4, 5] => 2
+
+numbers.delete(1) will delete any element that is equal to 1
+  [2, 3, 4, 5] => 1
+
+Question 5
+(10..100).cover?(42)
+
+Question 6
+famous_words = "seven years ago..."
+"Four score and " + famouse_words
+"Four score and ".concat(famous_words)
+famous_words.prepend("Four score and ")
+
+Question 7
+42
+
+Question 8
+flintstones.flatten!
+
+Question 9
+flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
+flintstones.assoc("Barney") # assoc searches through the hash with the KEY
+flintstones.rassoc(2) # rassoc searches through the hash with the VALUE
+
+=end
