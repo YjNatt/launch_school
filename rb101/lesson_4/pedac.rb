45b1d589720b7f135a8fea3f1f4939f22c2b2c81
Yujohn Nattrass <Yujohn.natt@gmail.com>
2019-07-28 19:40:34 -0700

    lesson 4 exercises

---
+++ rb101/lesson_4/pedac.rb
@@ -0,0 +1,15 @@
+=begin 
+Questions
+  what is a substring?
+  what is a palindrome?
+  will inputs always be strings
+  given an empty sting?
+  case sensitive?
+  how to return the substrings?
+
+input: string
+output: array of strings
+rules:
+    explicit requirements:
+      palindrome words are case sensitive
+      return only substrings which are palindromes
