161d8edac6e3950c3b2774cb864f66277b97e990
Yujohn <yujohn.natt@gmail.com>
2019-07-30 22:00:19 -0700

    easy 5 exercises

---
+++ rb101/small_problems/easy5/after_midnight1.rb
@@ -0,0 +1,8 @@
+=begin
+input: + or - integer
+output: string showing time in 24hr format
+rules: cannot use Date or Time classes
+       if number == positive the number is number of minutes after midnight
+       if number == negative the number is number of minute before midnight
+       return string in ("hh:mm") format
+data structure / algorithm
