781d0790dec9cc636e4998e1ddca6f688958d32c
Yujohn Nattrass <Yujohn.natt@gmail.com>
2019-08-22 07:26:31 -0700

    twenty one assignment update

--- rb101/lesson_6/twenty_one/twenty_one.rb
+++ rb101/lesson_6/twenty_one/twenty_one.rb
@@ -1,49 +1,47 @@
-=begin
-  deal_cards(deck)
-  cards_array = array
-  length of deck
-  delete_at random number between 0 and length of deck
-    save value to cards_array
-    return cards_array
-    
-=end
 CARDS = [ '2', '3', '4', '5', '6', '7', '8', '9', '10',
-               'Jack', 'Queen', 'King', 'Ace']
+         'Jack', 'Queen', 'King', 'Ace']
+
+SUITS = [ 'Heart', 'Spades', 'Diamonds', 'Clubs' ]
+
 FACE_CARD_VALUE = {'King' => 10, 'Queen' => 10, 'Jack' => 10, 'Ace' => [1, 11]}
 
+
 def Initialize_deck()
-  deck = []
-  4.times { deck << CARDS }
-  deck.flatten
+  full_deck = []
+  SUITS.each do |suit|
+    CARDS.each { |card| full_deck << [suit, card] }
+  end
+  full_deck.shuffle!
 end
 
-def deal_cards(dck)
+def deal_cards(deck)
   cards_dealt = []
-  how_many_cards = dck.length
-  2.times do
-    cards_dealt << dck.delete_at(rand(how_many_cards))
-  end
-
+  2.times { cards_dealt << deck.pop }
   cards_dealt
 end
 
 def cards_to_string(hand_arr)
-  if hand_arr.length == 2
-    hand_arr.join ' and '
-  elsif hand_arr.length > 2
-    hand_arr[0..-2].join(', ') + ' and ' + hand_arr[-1]
+  cards = []
+  hand_arr.each { |array| cards << array[1] }
+  if cards.length == 2
+    cards.join ' and '
   else
-    "#{hand_arr[0]} and unknown card"
+    cards[0..-2].join(', ') + ' and ' + cards[-1]
   end
 end
 
-def who_has_what(player_arr, dealer_arr)
-  puts "Dealer has: #{ cards_to_string(dealer_arr) }"
-  puts "Player has: #{ cards_to_string(player_arr) }"
+def who_has_what(player_arr, dealer_arr, end_game = false)
+  system "clear"
+  if end_game == false
+    puts "Dealer has: #{ dealer_arr.last.last } and unknown card"
+  else
+    puts "Dealer has: #{ cards_to_string(dealer_arr) }"
+  end
+    puts "Player has: #{ cards_to_string(player_arr) }"
 end
 
-def hit(hand, dck)
-  hand << dck.delete_at(rand(dck.length))
+def hit(hand, deck)
+  hand << deck.pop
 end
 
 def calculate_ace(points)
@@ -52,17 +50,17 @@ end
 
 def total(hand)
   total_points = 0
-  
+
   hand.each do |card|
-    if card.to_i > 0
-      total_points += card.to_i
-    elsif card != 'Ace'
-      total_points += FACE_CARD_VALUE[card]
+    if card[1].to_i > 0
+      total_points += card[1].to_i
+    elsif card[1] != 'Ace'
+      total_points += FACE_CARD_VALUE[card[1]]
     end
   end
 
   hand.each do |card|
-    if card == 'Ace'
+    if card[1] == 'Ace'
       total_points += calculate_ace(total_points)
     end
   end
@@ -70,27 +68,85 @@ def total(hand)
   total_points
 end
 
-def players_turn(players_hand, dealers_hand, dck)
+def busted?(hand)
+  points = total(hand)
+  return true if points > 21
+  false
+end
+
+def players_turn(players_hand, dealers_hand, deck)
   loop do
     puts 'hit or stay?'
     answer = gets.chomp
-    if answer.downcase != 'stay'
-      hit(players_hand, dck)
-      who_has_what(players_hand, dealers_hand)
-    else
-      break
-    end
+    hit(players_hand, deck) if answer.downcase == 'hit'
+    break if answer.downcase == 'stay' || busted?(players_hand)
+    who_has_what(players_hand, dealers_hand)
+  end
+
+  if busted?(players_hand)
+    who_has_what(players_hand, dealers_hand)
+    puts 'You have busted... Dealer wins!'
+  else
+    puts 'You chose to stay'
   end
 end
 
-deck = Initialize_deck()
+def dealers_turn(dealers_hand, deck)
+  loop do
+    hit(dealers_hand, deck) if total(dealers_hand) < 17
+    break if total(dealers_hand) >=17 || busted?(dealers_hand)
+  end
 
-player = deal_cards(deck) 
-dealer = deal_cards(deck)
+  puts 'Dealer has busted... Player wins!' if busted?(dealers_hand)
+end
 
-who_has_what(player, dealer)
-players_turn(player, dealer, deck)
-p total(player)
+def who_won?(players_hand, dealers_hand)
+  player_points = total(players_hand)
+  dealer_points = total(dealers_hand)
+
+  if player_points > dealer_points
+    'Player'
+  elsif player_points < dealer_points
+    'Dealer'
+  else
+    'Tie'
+  end
+end
+
+def display_who_won?(players_hand, dealers_hand)
+  winner = who_won?(players_hand, dealers_hand)
+  case winner
+  when 'Player' then puts 'Player has won the game'
+  when 'Dealer' then puts 'Dealer has won the game'
+  else               puts "It's a tie!"
+  end
+end
+
+loop do
+  deck = Initialize_deck()
+  player = deal_cards(deck) 
+  dealer = deal_cards(deck)
+  who_has_what(player, dealer)
+
+  loop do
+    players_turn(player, dealer, deck)
+    break if busted?(player)
+
+    dealers_turn(dealer, deck)
+    break if busted?(dealer)
+
+    who_has_what(player, dealer, true)
+    display_who_won?(player, dealer)
+    break
+  end
+  
+  puts "Would you like to play again? (y / n)"
+  answer = gets.chomp
+  break if answer.downcase.start_with?('n')
+end
+
+
+
 
 
 
