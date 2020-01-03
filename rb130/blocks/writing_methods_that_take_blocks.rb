aff659c1cb10af2b17a9f5d548ba00c4935cf5be
Yujohn Nattrass <Yujohn.natt@gmail.com>
2020-01-03 07:29:09 -0800

    Ruby 130 lesson

---
+++ rb130/blocks/writing_methods_that_take_blocks.rb
@@ -0,0 +1,36 @@
+def hello
+  "hello!"
+end
+
+def echo(str)
+  str
+end
+
+def echo_with_yield(str)
+  yield if block_given?
+  str
+end
+
+def say(words)
+  yield if block_given?
+  puts "> " + words
+end
+
+def increment(number)
+  if block_given?
+    yield(number + 1)
+  end
+  number + 1
+end
+
+def test
+  yield(1)
+end
+
+def compare(str)
+  puts "Before: #{str}"
+  after = yield(str)
+  puts "After: #{after}"
+end
+
+compare('hello') { |word| puts "hi" }
