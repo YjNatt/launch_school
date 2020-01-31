03dc14177b8e94229b18b67bd1ac664e51f91660
Yujohn Nattrass <Yujohn.natt@gmail.com>
2020-01-31 08:50:42 -0800

    ruby challenges easy 1

--- ruby_challenges/easy_1/series.rb
+++ ruby_challenges/easy_1/series.rb
@@ -6,8 +6,18 @@ class Series
   def slices(num)
     raise ArgumentError.new('Number is greater than legnth of string') if num > @string_of_digits.length
     slices = [];
-    @string_of_digits.chars.each_cons(num) do |nums|
-      slices << nums.map(&:to_i)      
+    # @string_of_digits.chars.each_cons(num) do |nums|
+    #   slices << nums.map(&:to_i)      
+    # end
+
+    index = 0
+    digits = @string_of_digits.chars.map(&:to_i)
+
+    loop do
+      sliced_digits = digits[index, num]
+      break if sliced_digits.length < num
+      slices << sliced_digits
+      index += 1
     end
     slices
   end
