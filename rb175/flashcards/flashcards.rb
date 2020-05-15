4d1cedd810489dc914d372b3c3bed877c2cd1bf4
Yujohn <yujohn.natt@gmail.com>
2020-05-15 07:16:07 -0700

    method to load users data from users.yml

--- rb175/flashcards/flashcards.rb
+++ rb175/flashcards/flashcards.rb
@@ -5,6 +5,17 @@ require "tilt/erubis"
 require "yaml"
 require "bcrypt"
 
+def load_user_credentials
+  credentials_path = if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/users.yml", __FILE__)
+  else
+    File.expand_path("../users.yml", __FILE__)
+  end
+
+  YAML.load_file(credentials_path) || {}
+end
+
 get "/" do
   erb :index
 end
+
