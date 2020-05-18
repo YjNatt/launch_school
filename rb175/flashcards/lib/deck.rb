42ce0675cad905f18cf0b13b7cbf11d55e2693f0
Yujohn <yujohn.natt@gmail.com>
2020-05-18 10:16:55 -0700

    fix getter methods

--- rb175/flashcards/lib/deck.rb
+++ rb175/flashcards/lib/deck.rb
@@ -1,6 +1,6 @@
 class Deck
   attr_accessor :name
-  attr_reader :questions, :answered
+  attr_reader :answered_flashcards, :unanswered_flashcards
 
   def initialize(name)
     @name = name
@@ -8,7 +8,16 @@ class Deck
     @unanswered_flashcards = []
   end
 
-  def add_flashcard(id, flashcard)
+  def add(id, flashcard)
     @unanswered_flashcards << { id => flashcard }
+  end
+
+  def flashcards
+    flashcards = @answered_flashcards + @unanswered_flashcards
+
+    flashcards.each_with_object({}) do |card, hsh|
+      id, flashcard = card.to_a.first
+      hsh[id] = flashcard
+    end
   end
 end
