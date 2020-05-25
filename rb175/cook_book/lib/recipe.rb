fb52a30b94c3abe5fc4f69a78088b938531a59df
Yujohn <yujohn.natt@gmail.com>
2020-05-25 13:52:44 -0700

    create methods to add, edit, or delete ingredients and steps to recipes

---
+++ rb175/cook_book/lib/recipe.rb
@@ -0,0 +1,29 @@
+class Recipe
+  attr_reader :name, :ingredients, :steps
+
+  def initialize(name)
+    @name = name
+    @ingredients = {}
+    @steps = {}
+  end
+
+  def add_ingredient(id, ingredient)
+    @ingredients[id] = ingredient
+  end
+
+  alias :edit_ingredient :add_ingredient
+
+  def delete_ingredient(id)
+    @ingredients.delete(id)
+  end
+
+  def add_step(id, step)
+    @steps[id] = step
+  end
+
+  alias :edit_step :add_step
+
+  def delete_step(id)
+    @steps.delete(id)
+  end
+end
