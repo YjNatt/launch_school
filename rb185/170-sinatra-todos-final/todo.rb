d5fa3dd0d6827d75e8d07192d3b7256911bdb19e
Yujohn <yujohn.natt@gmail.com>
2020-06-29 06:48:36 -0700

    fix minor bug

--- rb185/170-sinatra-todos-final/todo.rb
+++ rb185/170-sinatra-todos-final/todo.rb
@@ -1,12 +1,18 @@
 require "sinatra"
-require "sinatra/reloader"
 require "sinatra/content_for"
 require "tilt/erubis"
+
+require_relative "database_persistence"
 
 configure do
   enable :sessions
   set :session_secret, 'secret'
   set :erb, :escape_html => true
+end
+
+configure(:development) do
+  require "sinatra/reloader"
+  also_reload "database_persistence.rb"
 end
 
 helpers do
@@ -41,70 +47,6 @@ helpers do
   end
 end
 
-class SessionPersistence
-  def initialize(session)
-    @session = session
-    @session[:lists] ||= []
-  end
-
-  def find_list(id)
-    @session[:lists].find{ |list| list[:id] == id }
-  end
-
-  def all_lists
-    @session[:lists]
-  end
-
-  def lists
-    @session[:lists]
-  end
-
-  def delete_list(id)
-    @session[:lists].reject! { |list| list[:id] == id }
-  end
-
-  def create_new_list(list_name)
-    id = next_element_id(@session[:lists])
-    @session[:lists] << { id: id, name: list_name, todos: [] }
-  end
-
-  def update_list_name(list_id, new_name)
-    list = find_list(list_id)
-    list[:name] = new_name
-  end
-
-  def create_new_todo(list_id, todo_name)
-    list = find_list(list_id)
-    id = next_element_id(list[:todos])
-    list[:todos] << { id: id, name: todo_name, completed: false }
-  end
-
-  def delete_todo_from_list(list_id, todo_id)
-    list = find_list(list_id)
-    list[:todos].reject! { |todo| todo[:id] == todo_id }
-  end
-
-  def update_todo_status(list_id, todo_id, new_status)
-    list = find_list(list_id)
-    todo = list[:todos].find { |todo| todo[:id] == todo_id }
-    todo[:completed] = new_status
-  end
-
-  def mark_all_todos_as_completed(list_id)
-    list = find_list(list_id)
-    list[:todos].each do |todo|
-      todo[:completed] = true
-    end
-  end
-
-  private
-
-  def next_element_id(elements)
-    max = elements.map { |todo| todo[:id] }.max || 0
-    max + 1
-  end
-end
-
 def load_list(id)
   list = @storage.find_list(id)
   return list if list
@@ -130,7 +72,7 @@ def error_for_todo(name)
 end
 
 before do
-  @storage = SessionPersistence.new(session)
+  @storage = DatabasePersistence.new(logger)
 end
 
 get "/" do
@@ -139,7 +81,7 @@ end
 
 # View list of lists
 get "/lists" do
-  @lists = @storage.lists
+  @lists = @storage.all_lists
   erb :lists, layout: :layout
 end
 
@@ -257,7 +199,7 @@ post "/lists/:id/complete_all" do
   @list_id = params[:id].to_i
   @list = load_list(@list_id)
 
-  @storage.mark_all_todos_as_completed(list_id)
+  @storage.mark_all_todos_as_completed(@list_id)
 
   session[:success] = "All todos have been completed."
   redirect "/lists/#{@list_id}"
